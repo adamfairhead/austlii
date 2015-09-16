1c9115ffdfff405ffd5397cf4548dc26ec4f4f79
Adam Fairhead <adam@adamfairhead.com>
2015-09-16 10:43:59 -0400

    Update panel year option selector with choice

--- js/production.js
+++ js/production.js
@@ -32,38 +32,21 @@ $(function() {
     return false;
   });
 
-  // Select 'Date Range #1' panel dropdown
-  $('.date-range-dropdown-1').on('click', function() {
-    $('.year-options').removeClass('is-visible');
-    $('.date-range-1-options').addClass('is-visible');
-    $('#panel-year .panel-type-all .is-selected').removeClass('is-selected');
-    $(this).addClass('is-selected').parent().removeClass('is-selected');
-    return false;
-  });
-
-  // Select 'Date Range #2' panel dropdown
-  $('.date-range-dropdown-2').on('click', function() {
-    $('.year-options').removeClass('is-visible');
-    $('.date-range-2-options').addClass('is-visible');
-    $('#panel-year .panel-type-all .is-selected').removeClass('is-selected');
-    $(this).addClass('is-selected').parent().removeClass('is-selected');
-    return false;
-  });
-
-  // Select 'Date Range #3' panel dropdown
-  $('.date-range-dropdown-3').on('click', function() {
-    $('.year-options').removeClass('is-visible');
-    $('.year-range-2-options').addClass('is-visible');
-    $(this).parent().parent().find('.is-selected').removeClass('is-selected');
-    $(this).addClass('is-selected').parent().removeClass('is-selected');
-    return false;
-  });
-
   // Hide the year options dropdown when a year is selected
   $('.year-options a').on('click', function() {
     $('.year-options.is-visible').removeClass('is-visible');
     $('#panel-year a.is-selected').removeClass('is-selected');
-    return false;
+  });
+
+  // Move year option selection over to the panel selector
+  $('.year-options a').on('click', function() {
+    if( $(this).parent().parent().parent().parent().hasClass('year-specific-options') ) {
+      $('.year-specific-dropdown').text($(this).text());
+    } else if( $(this).parent().parent().parent().parent().hasClass('year-range-1-options') ) {
+      $('.year-range-dropdown-1').text($(this).text());
+    } else if( $(this).parent().parent().parent().parent().hasClass('year-range-2-options') ) {
+      $('.year-range-dropdown-2').text($(this).text());
+    };
   });
 });
 
