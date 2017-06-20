1a98dd6567da762583007ed8ea318b1308e6362d
dcerimagic <denis.cerimagic@fairheadcreative.com>
2017-06-20 16:36:36 +0200

    Added credit card icons to Contribute page

--- js/layout/switch.js
+++ js/layout/switch.js
@@ -14,14 +14,23 @@ $(function () {
   
   $('[data-method="method1"]').on('click', function () {
     $('.meta-method').text('Pressing continue will take you to an online payment gateway.');
+    if ($('.contribute-cards').hasClass("hidden")) {
+      $('.contribute-cards').removeClass('hidden');
+    }
   });
   
   $('[data-method="method2"]').on('click', function () {
     $('.meta-method').text('Payable to "AustLII Foundation Limited" (ABN 41 134 717 972).');
+    if (!$('.contribute-cards').hasClass("hidden")) {
+      $('.contribute-cards').addClass('hidden');
+    }
   });
   
   $('[data-method="method3"]').on('click', function () {
     $('.meta-method').text('An invoice will be sent to the above adress.');
+    if (!$('.contribute-cards').hasClass("hidden")) {
+      $('.contribute-cards').addClass('hidden');
+    }
   });
 
 });
