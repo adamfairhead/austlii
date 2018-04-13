5a01c81f603be95953f9a38f68f29957e9b745f6
Fedor Sherbakov <mail@eqape.com>
2018-04-13 12:20:30 +0300

    Add watch script

--- gruntfile.js
+++ gruntfile.js
@@ -24,11 +24,22 @@ module.exports = function(grunt) {
           'js/production.min.js': ['<%= concat.dist.dest %>']
         }
       }
-    } 
+    },
+
+    watch: {
+      scripts: {
+        files: ['js/layout/*.js'],
+        tasks: ['default'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', ['concat', 'uglify']);
 };
